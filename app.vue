ed4b3183bea124c7ee4f8cd9056eda55d3a447b2
John Henry Galino <jhgalino@pop-os.localdomain>
2023-03-30 04:59:59 +0800

    initial

---
+++ app.vue
@@ -0,0 +1,12 @@
+<template>
+  <Navbar />
+  <NuxtPage />
+</template>
+
+<script setup>
+let body = useHeadSafe({
+  bodyAttrs: {
+    class: "text-gray-300 bg-neutral-800 leading-6"
+  }
+})
+</script>
---
+++ shell.nix
@@ -0,0 +1,7 @@
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/refs/tags/22.11.tar.gz") {}}:
+
+pkgs.mkShell {
+    buildInputs = [
+        pkgs.nodejs
+    ];
+}
