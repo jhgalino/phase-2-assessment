604cc87cd196f1c1ceb5a883a094912f8834c471
John Henry Galino <github.stolze@simplelogin.com>
2023-04-02 12:39:12 +0800

    remove some spaces

--- components/filters/filtersBox.vue
+++ components/filters/filtersBox.vue
@@ -1,24 +1,20 @@
 <template>
   <section class="w-full mb-6 rounded-md bg-neutral-700">
-    
-    <!-- Dropdown List -->
-   
-      <slot name="select"></slot>
+    <slot name="select"></slot>
 
     <section class="px-6 pb-6">
       <p class="my-3 text-2xl font-bold">{{ title }}</p>
       <hr>
       <div class="overflow-y-scroll max-h-40">
-        <slot name="choices"></slot>
+        <FiltersBoxChoices :items="items" />
       </div>
-
     </section>
-
   </section>
 </template>
 
 <script setup>
 let props = defineProps({
-  title: String
+  title: String,
+  items: Array,
 })
 </script>
